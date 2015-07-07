a9fd67dca1b2db97ff290de3aa57295a0aa8d8f5
John Weldon <johnweldon4@gmail.com>
2015-07-07 09:53:48 -0700

    Format output

--- cmd/nginx-log-util/main.go
+++ cmd/nginx-log-util/main.go
@@ -10,5 +10,7 @@ import (
 func main() {
 	p := util.NewParser(os.Stdin)
 	records := p.GetRecords()
-	fmt.Fprintf(os.Stdout, "%#v\n", records)
+	for _, record := range records {
+		fmt.Fprintf(os.Stdout, "%20s %16s %d %-40s %s\n", record.TimeLocal.Format("2006-01-02 15:04:05"), record.RemoteAddr, record.Status, record.Request, record.HttpReferrer)
+	}
 }
